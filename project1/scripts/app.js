a7b0e1772c92d20ab03cd34c4e7c4a9c035e79d5
Katherine Ude <Katie.Ude@gmail.com>
2015-11-10 23:37:19 -0600

    saving project work for the night

--- project1/scripts/app.js
+++ project1/scripts/app.js
@@ -7,13 +7,20 @@ gameStart();
 var player1;
 var player2;
 var shapes = [
-          'Square',
-          'Circle',
-          'Diamond',
-          'Fleur',
-          'Heart',
-          'Clover'
+          'square',
+          'circle',
+          'diamond',
+          'fleur',
+          'heart',
+          'clover'
         ];
+// var square = $('#wrapper div:eq(1)');
+// var circle = $('#wrapper div:eq(2)');
+// var diamond = $('#wrapper div:eq(3)');
+// var fleur = $('#wrapper div:eq(4)');
+// var heart = $('#wrapper div:eq(5)');
+// var clover = $('#wrapper div:eq(6)');
+
 
 function gameStart(){
   // fade's in door
@@ -46,7 +53,7 @@ function createPlayer() {
   var userInput = prompt('Please enter the name for Player One.');
     if (userInput != undefined) {
       var player1 = userInput;
-      alert('Thank you ' + player1 + 'this is player1');
+      alert('Thank you ' + player1);
     }
 
     var userInput2 = prompt('Please enter the name for Player Two.');
@@ -59,8 +66,49 @@ function createPlayer() {
 
 function buildDoor(){
   for(var i = 0; i < shapes.length; i++) {
-    console.log('my loop is working');
-    var shapeNames = "<div>" + shapes[i] + "</div>";
+    // console.log('my loop is working');
+    var shapeNames = "<div></div>";
     $('#wrapper').append(shapeNames);
   }
-}
+    var square = $('#wrapper div:eq(1)');
+    square.attr('id', 'square');
+    var circle = $('#wrapper div:eq(2)');
+    circle.attr('id', 'circle');
+    var diamond = $('#wrapper div:eq(3)');
+    diamond.attr('id', 'diamond');
+    var fleur = $('#wrapper div:eq(4)');
+    fleur.attr('id', 'fleur');
+    var heart = $('#wrapper div:eq(5)');
+    heart.attr('id', 'heart');
+    var clover = $('#wrapper div:eq(6)');
+    clover.attr('id', 'clover');
+  clickShapes();
+} //end buildDoor function
+
+function clickShapes() {
+$("#square").on("click", function(){
+  $(this).css('background', 'rgb(47, 75, 224)');
+  $("#square.background").fadeOut("slow");
+  console.log('this square click works!!!');
+});
+$("#circle").on("click", function(){
+  $(this).css('background', 'rgb(47, 75, 224)');
+  console.log('this cicle click works!!!');
+});
+$("#diamond").on("click", function(){
+  $(this).css('background', 'rgb(47, 75, 224)');
+  console.log('this diamond click works!!!');
+  });
+$("#fleur").on("click", function(){
+  $(this).css('background', 'rgb(47, 75, 224)');
+      console.log('this fleur click works!!!');
+  });
+$("#heart").on("click", function(){
+  $(this).css('background', 'rgb(47, 75, 224)');
+    console.log('this heart click works!!!');
+  });
+$("#clover").on("click", function(){
+  $(this).css('background', 'rgb(47, 75, 224)');
+    console.log('this clover click works!!!');
+  });
+} //end of clickShapes function
