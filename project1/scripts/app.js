3d0264aebec6d76e363123f4fcbd11be7ad1aae2
Katherine Ude <Katie.Ude@gmail.com>
2015-11-10 18:36:38 -0600

    saving lots of project fixes

--- project1/scripts/app.js
+++ project1/scripts/app.js
@@ -1,44 +1,66 @@
-$(document).ready(function(){
+$('document').ready(function(){
 
 gameStart();
 
 }); //end of doc.ready function
 
+var player1;
+var player2;
+var shapes = [
+          'Square',
+          'Circle',
+          'Diamond',
+          'Fleur',
+          'Heart',
+          'Clover'
+        ];
+
 function gameStart(){
-$('#start_button').click(function(){
-  $('h1, #main').fadeOut("slow",function() {
-    $('#wrapper').css({'background-image': 'url(http://imgur.com/rgBCXTH.jpg)'});
-    $('#wrapper').fadeIn(400).delay(400).promise().done(function createPlayer1(name) {
-          var userInput = prompt('Please enter the name for Player One.');
-                if (userInput == 'name') {
-                  alert('Thank you ' + name);
-                  var Player1 = name;
-                }
-            })
-            if (player1 = 'true') {
-            function createPlayer2() {
-            var userInput2 = prompt('Please enter the name for Player Two.');
-              var player2 = userInput2;
-                if (userInput2 !== null||'') {
-                alert('Thank you ' + player2);
-              }
-              }
-            }
-          })
-        })
-      }
-// var player1 = {
-//   name: '',
-//   score: '',
-// createPlayer1: function() {
-// var userInput = prompt('Please enter the name for Player One.');
-// if (userInput !== null||'') {
-//   player1.name == userInput;
-//   alert('Thank you' + player1.name);
-//       }
-// else  {
-//   alert('Please try re-entering Player one\'s name');
-//       }
-//     }
-//   }
-// }
+  // fade's in door
+  // prompts user to enter player one name
+  //
+  $('#start_button').click(function(){
+    $('h1').fadeOut("slow");
+    $('#main').fadeOut("slow",function() {
+      console.log('end of fade out');
+      initWrapper();
+    }); //start_button CLICK end of function
+  });
+}
+      // $('#wrapper').fadeIn(400).delay(400);
+      // }) promise done end of function
+  //  buildDoor();
+ // gameStart end of function
+
+function initWrapper(){
+  $('#wrapper').css('background-image', 'url(http://imgur.com/rgBCXTH.jpg)');
+  $('#wrapper').css('display', 'none');
+  $('#wrapper').fadeIn(1000, function(){
+    //console.log('end of fade in');
+    createPlayer();
+  });
+
+}
+
+function createPlayer() {
+  var userInput = prompt('Please enter the name for Player One.');
+    if (userInput != undefined) {
+      var player1 = userInput;
+      alert('Thank you ' + player1 + 'this is player1');
+    }
+
+    var userInput2 = prompt('Please enter the name for Player Two.');
+    if (userInput2 != undefined) {
+      var player2 = userInput2;
+      alert('Thank you ' + player2);
+    }
+    buildDoor();
+} //createPlayer end of function
+
+function buildDoor(){
+  for(var i = 0; i < shapes.length; i++) {
+    console.log('my loop is working');
+    var shapeNames = "<div>" + shapes[i] + "</div>";
+    $('#wrapper').append(shapeNames);
+  }
+}
